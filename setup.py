acf205513526d1d68db40a70a472577afcbbdb52
Martin Sivak <msivak@redhat.com>
2008-11-25 13:20:10 +0100

    Link with libcryptsetup.so in current directory

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 setup(name="cryptsetup", version="1.0",
-              ext_modules=[Extension("cryptsetup", ["cryptsetup.c"])])
+              ext_modules=[Extension("cryptsetup", ["cryptsetup.c"], library_dirs=['.'], libraries=['cryptsetup'])])
 
