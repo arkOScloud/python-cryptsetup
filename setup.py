dada13145e77c6957a296fa0f7999ece6380f2a6
Martin Sivak <msivak@redhat.com>
2008-11-25 12:26:25 +0100

    Use the .c file in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 setup(name="cryptsetup", version="1.0",
-              ext_modules=[Extension("cryptsetup", ["cryptsetup.cc"])])
+              ext_modules=[Extension("cryptsetup", ["cryptsetup.c"])])
 
