9878965ee9eb0f61b67ce491832c1c45a334b349
Martin Sivak <msivak@redhat.com>
2009-03-05 13:07:17 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 setup(name="pycryptsetup",
-              version = '0.0.6',
+              version = '0.0.7',
               description = "Python bindings for cryptsetup",
               author = "Martin Sivak",
               author_email= "msivak@redhat.com",
