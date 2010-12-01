5de1a421cfbade80a2fcc71f50d0d45b45b0d52c
Martin Sivak <msivak@redhat.com>
2010-12-01 14:06:29 +0100

    Fix version in setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 setup(name="pycryptsetup",
-              version = '0.0.11',
+              version = '0.1.0',
               description = "Python bindings for cryptsetup",
               author = "Martin Sivak",
               author_email= "msivak@redhat.com",
